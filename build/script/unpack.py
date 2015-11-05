7a44f7e16572809b7e3793c4b1903bde927556ab
AddRef <rwolfs@inbox.ru>
2015-11-05 18:07:46 +0300

    Enabled option to skip caching of archive output dir
    Script will not unpack archive again if output folder has changed since
    last unpack

--- build/script/unpack.py
+++ build/script/unpack.py
@@ -8,12 +8,14 @@ import os
 import shutil
 import xml.etree.ElementTree as etree
 from optparse import OptionParser
-from collections import namedtuple
 
 g_log = debug.Log()
 g_log.enable(debug.LogType.Info, True)
 g_log.enable(debug.LogType.Error, True)
 g_log.enable(debug.LogType.Debug, True)
+
+class UnpackConfigEntry:
+    cache_destination = True
 
 class UnpackConfig:
     """Filters list of files based on unpack_config.xml"""
@@ -46,28 +48,33 @@ class UnpackConfig:
             return False
         return True
 
+    def get_entry_config(self, file_name):
+        if file_name in self._filters:
+            return self._filters[file_name]
+        return None
+
     def _process_config(self, config):
         for module in config:
             # Read attributes
-            filter_entry = namedtuple('FilterEntry', ['dest_cache'])
-            if 'dest_cache' in module.attrib:
-                filter_entry.dest_cache = module.attrib['dest_cache']
+            filter_entry = UnpackConfigEntry()
+            if 'cache_destination' in module.attrib and module.attrib['cache_destination'] == 'disabled':
+                filter_entry.cache_destination = False
             self._filters[module.tag] = filter_entry
 
 class Unpacker:
     def __init__(self):
         pass
 
-    def unpack_dir(self, input_dir, output_dir, cache = None, config = None):
+    def unpack_dir(self, input_dir, output_dir, cache=None, config=None):
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
         for input_file in os.listdir(input_dir):
             name, __ = utils.splitext(input_file)
             input_file = os.path.join(input_dir, input_file)
             if not config or config.needs_process(name):
-                self.unpack_file(input_file, output_dir, cache)
+                self.unpack_file(input_file, output_dir, cache, config.get_entry_config(name))
 
-    def unpack_file(self, input_file, output_dir, cache = None):
+    def unpack_file(self, input_file, output_dir, cache=None, config_entry=None):
         input_file = os.path.abspath(input_file)
         output_dir = os.path.abspath(output_dir)
         if not os.path.isfile(input_file):
@@ -76,18 +83,20 @@ class Unpacker:
         if not archive.is_supported_archive(input_file):
             g_log.error("Unsupported archive %s" % input_file)
             return
-        if not self._file_requires_unpack(input_file, output_dir, cache):
+        if not self._file_requires_unpack(input_file, output_dir, cache, config_entry):
             g_log.info("Skipping file %s: it was already unpacked" % input_file)
             return
         self._unpack_file(input_file, output_dir)
-        self._update_cache(input_file, output_dir, cache)
+        self._update_cache(input_file, output_dir, cache, config_entry)
 
         
-    def _file_requires_unpack(self, input_file, output_dir, cache):
+    def _file_requires_unpack(self, input_file, output_dir, cache, config_entry):
         if cache:
             output_dir = archive.get_output_path(input_file, output_dir)
             input_file_changed = cache.entry_has_changed(input_file)
-            output_dir_changed = os.path.exists(output_dir) and cache.entry_has_changed(output_dir)
+            output_dir_changed = False
+            if not config_entry or config_entry.cache_destination:
+                output_dir_changed = not os.path.exists(output_dir) or cache.entry_has_changed(output_dir)
             return input_file_changed or output_dir_changed
         return True
 
@@ -97,9 +106,10 @@ class Unpacker:
         archive.unpack(input_file, output_dir)
         g_log.info("Extracting %s... DONE" % (input_file))                    
 
-    def _update_cache(self, input_file, output_dir, cache):
+    def _update_cache(self, input_file, output_dir, cache, config_entry):
         if cache:
-            cache.update_entry(archive.get_output_path(input_file, output_dir))
+            if not config_entry or config_entry.cache_destination: 
+                cache.update_entry(archive.get_output_path(input_file, output_dir))
             cache.update_entry(input_file)
             cache.flush()
 
@@ -110,7 +120,7 @@ class Unpacker:
 
 if __name__ == '__main__':
     parser = OptionParser()
-    parser.add_option('-d', '--input', dest='input', help='input directory or file', default='.')
+    parser.add_option('-i', '--input', dest='input', help='input directory or file', default='.')
     parser.add_option('-o', '--output', dest='output', help='output directory', default='./_unpack')
     parser.add_option('-c', '--config', dest='config', help='configuration file containing packages to unpack', default='./unpack_config.xml')
     (options, args) = parser.parse_args()
