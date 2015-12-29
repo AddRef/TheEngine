f41e9102b5e12bccee4ca00de8a7c76a534fd0c1
AddRef <rwolfs@inbox.ru>
2015-12-29 18:57:58 +0300

    callbacks implementation

--- src/utils/hxx/callbacks.hxx
+++ src/utils/hxx/callbacks.hxx
@@ -1,45 +1,131 @@
 #pragma once
 #include <memory>
-#include <set>
+#include <list>
 
 template <typename TCallback>
 class Receiver
     : public TCallback
     , protected std::enable_shared_from_this<TCallback>
 {
-    template <typename T>
     friend class Emitter;
-    
+
     using Ptr = std::shared_ptr<TCallback>;
-    using WPtr = std::WPtr<TCallback>;
-    
-    Ptr& GetPtr()
+    using WPtr = std::weak_ptr<TCallback>;
+
+    Ptr _getPtr()
     {
         return this->shared_from_this();
     }
-    
+
 };
 
-
-template <typename TCallback>
 class Emitter
 {
-public:
-    void AddCallback(Receiver<TCallback>& receiver)
+    // Holder to keep Emitter non-template class. Instead individual functions are templated.
+    // That requires if you want to define list of callbacks inside class deriverd from Emitter
+    struct IHolder
     {
-        m_callbacks.insert(receiver.GetPtr());
-    }
-    
-    void RemoveCallback(Receiver<TCallback>& receiver)
+        using UPtr = std::unique_ptr<IHolder>;
+        virtual ~IHolder() {}
+    };
+    template <typename TCallback>
+    class Holder : public IHolder
     {
-        if (!m_callbacks.empty())
+    public:
+        typedef TCallback TCallback;
+        virtual ~Holder() {}
+        virtual void AddCallback(Receiver<TCallback>& receiver)
         {
-            m_callbacks.erase(receiver.GetPtr());
+            m_callbacks.push_back(receiver._getPtr());
+        }
+        virtual void RemoveCallback(Receiver<TCallback>& receiver)
+        {
+            for (auto iter = m_callbacks.begin(); iter != m_callbacks.end(); ++iter)
+            {
+                Receiver<TCallback>::Ptr ptr = iter->lock();
+                if (ptr == receiver._getPtr())
+                {
+                    iter = m_callbacks.erase(iter);
+                }
+            }
+        }
+        std::list<TCallback*> GetCallback()
+        {
+            std::list<TCallback*> callbacks;
+            for (auto iter = m_callbacks.begin(); iter != m_callbacks.end(); ++iter)
+            {
+                Receiver<TCallback>::Ptr callback = iter->lock();
+                if (callback)
+                {
+                    callbacks.push_back(callback.get());
+                }
+                else
+                {
+                    iter = m_callbacks.erase(iter);
+                }
+            }
+            return callbacks;
+        }
+    private:
+        std::list< typename Receiver<TCallback>::WPtr > m_callbacks;
+    };
+
+public:
+    Emitter()
+    {
+    }
+
+    // IMPORTANT: Don't forget to call this function before using other stuff
+    // This is a cost for keeping Emitter non-template class
+    template <typename TCallback>
+    void Init()
+    {
+        auto existing_holder = _findHolder<TCallback>();
+        if (!existing_holder)
+        {
+            IHolder::UPtr holder(new Holder<TCallback>());
+            m_holders.push_back(std::move(holder));
         }
     }
-    
+
+    template <typename TCallback>
+    void AddCallback(Receiver<TCallback>& receiver)
+    {
+        auto holder = _findHolder<TCallback>();
+        if (!holder) throw std::runtime_error("Invalid callback type is passed");
+        holder->AddCallback(receiver);
+    }
+
+    template <typename TCallback>
+    void RemoveCallback(Receiver<TCallback>& receiver) 
+    {
+        auto holder = _findHolder<TCallback>();
+        if (!holder) throw std::runtime_error("Invalid callback type is passed");
+        holder->RemoveCallback(receiver);
+    }
+
+    template <typename TCallback>
+    std::list<TCallback*> GetCallback()
+    {
+        auto holder = _findHolder<TCallback>();
+        if (!holder) return std::list<TCallback*>();
+        return holder->GetCallback();
+    }
+
 private:
-    std::set< typename Receiver<TCallback>::WPtr > m_callbacks;
+    template <typename TCallback>
+    Holder<TCallback>* _findHolder()
+    {
+        for (const IHolder::UPtr& holder : m_holders)
+        {
+            Holder<TCallback>* typed_holder = dynamic_cast<Holder<TCallback>*>(holder.get());
+            if (typed_holder)
+            {
+                return typed_holder;
+            }
+        }
+        return nullptr;
+    }
 
+    std::list<IHolder::UPtr> m_holders;
 };
-
--- src/utils/hxx/test/callbacks_test.cpp
+++ src/utils/hxx/test/callbacks_test.cpp
@@ -2,20 +2,61 @@
 
 #include "gtest/gtest.h"
 
+#include <functional>
+#include <sstream>
 
-class BroadcasterImpl : protected Broadcaster<BroadcasterImpl::ICallback>
+using TFunction = std::function<void(const std::string& str)>;
+
+class EmitterImpl : public Emitter
 {
 public:
-    struct ICallback
+    struct ICallback0
     {
-
+        virtual void MyCallback0(TFunction function) = 0;
     };
 
+    struct ICallback1
+    {
+        virtual void MyCallback1(TFunction function) = 0;
+    };
+
+   EmitterImpl::EmitterImpl()
+   {
+       Emitter::Init<ICallback0>();
+       Emitter::Init<ICallback1>();
+   }
 };
 
-class BroadcastCallbackImpl : protected Broadcaster<BroadcasterImpl::ICallback>::Callback
+class Receiver0Impl : public Receiver<EmitterImpl::ICallback0>
 {
+public:
+    Receiver0Impl(const char* str)
+    {
+        m_string = str ? str : "";
+    }
+    virtual void MyCallback0(TFunction function) override
+    {
+        if (function)
+            function(m_string);
+    }
+private:
+    std::string m_string;
+};
 
+class Receiver1Impl : public Receiver<EmitterImpl::ICallback1>
+{
+public:
+    Receiver1Impl(const char* str)
+    {
+        m_string = str ? str : "";
+    }
+    virtual void MyCallback1(TFunction function) override
+    {
+        if (function)
+            function(m_string);
+    }
+private:
+    std::string m_string;
 };
 
 class CallbacksTest : public ::testing::Test
@@ -39,27 +80,52 @@ public:
     {
     }
 private:
-    Broadcaster m_broadcaster;
-    BroadcastCallback m_broadcast_callback
 };
 
-class EmitterImpl : public Emitter<EmitterImpl, EmitterImpl::ICallback>
+TEST_F(CallbacksTest, CallbacksTest_MultipleEqualCallbacks)
 {
-public:
-    struct ICallback
+    std::vector<std::string> stream;
+    EmitterImpl emitter;
+    auto function = [&stream](const std::string& str) { stream.push_back(str); };
+    Receiver0Impl receiver0("receiver0");
+    Receiver0Impl receiver1("receiver1");
+    emitter.AddCallback(receiver0);
+    emitter.AddCallback(receiver1);
+    auto callbacks0 = emitter.GetCallback<EmitterImpl::ICallback0>();
+    auto callbacks1 = emitter.GetCallback<EmitterImpl::ICallback1>();
+    EXPECT_EQ(callbacks0.size(), 2);
+    EXPECT_EQ(callbacks1.size(), 0);
+    for (EmitterImpl::ICallback0* callback : callbacks0)
     {
-        virtual void Callback() = 0;
-    };
-};
-
-class RecieverImpl : protected EmitterImpl::ICallback
-{
-public:
-    virtual void Callback() override
-    {
-        std::cout << "Callback" << std::endl;
+        callback->MyCallback0(function);
     }
-};
+    EXPECT_EQ(stream.size(), 2);
+    EXPECT_EQ(stream[0], "receiver0");
+    EXPECT_EQ(stream[1], "receiver1");
+}
+
+TEST_F(CallbacksTest, CallbacksTest_DeadCallback)
+{
+    std::vector<std::string> stream;
+    EmitterImpl emitter;
+    auto function = [&stream](const std::string& str) { stream.push_back(str); };
+    Receiver0Impl receiver0("receiver0");
+    {
+        Receiver0Impl receiver1("receiver1");
+        emitter.AddCallback(receiver0);
+        emitter.AddCallback(receiver1);
+    }
+    auto callbacks0 = emitter.GetCallback<EmitterImpl::ICallback0>();
+    auto callbacks1 = emitter.GetCallback<EmitterImpl::ICallback1>();
+    EXPECT_EQ(callbacks0.size(), 1);
+    EXPECT_EQ(callbacks1.size(), 0);
+    for (EmitterImpl::ICallback0* callback : callbacks0)
+    {
+        callback->MyCallback0(function);
+    }
+    EXPECT_EQ(stream.size(), 1);
+    EXPECT_EQ(stream[0], "receiver0");
+}
 
 int main(int argc, char **argv) 
 {
